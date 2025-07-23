12315e39ed3c3f1f04d80d5d429733f2e24e159e
YManagan <61014073+gladbio@users.noreply.github.com>
2025-07-22 22:33:18 -0300

    Melhorar a função de conversão de GIFs
    fix: executa corretamente fora da pasta src/

--- src/app.py
+++ src/app.py
@@ -1,23 +1,52 @@
-# TODO: multithreading (pra cada usuário), compartilhamento de recurso (a conversão ta sendo feita nas mesmas threads q o aplicativo roda), tratamento de erro
+# TODO: multithreading (pra cada usuário), compartilhamento de recurso (a conversão ta sendo feita nas mesmas threads q o aplicativo roda), tratamento de erro (importante pq ta péssimo)
 
-import secrets, os
+import secrets, os, subprocess, sys
 from werkzeug.utils import secure_filename
 from flask import Flask, render_template, request, send_file, redirect, url_for
-from pathlib import PureWindowsPath, PurePosixPath
+from pathlib import Path
 
 app = Flask(__name__)
 app.config['GIF_UPLOAD_FOLDER'] = "./gifs"
 app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024 # 20MB
 app.config['USED_STORAGE'] = 0
 app.config['MAX_STORAGE_SIZE'] = 500 * 1024 * 1024 # 500MB
+GIFSKI_PATH = "gifski-1_32_0" if os.name == "posix" else "..\\bin\\gifski-1_32_0.exe"
+BASE_DIR = Path(__file__).resolve().parent
 
-def videoToGif(videoFilePath, convertedFilename):
-    if(os.name == "posix"):
-        os.system( str( PurePosixPath(f"ffmpeg -i {videoFilePath} -v 0 -f yuv4mpegpipe - | gifski-1_32_0 -o {app.config['GIF_UPLOAD_FOLDER']}/{convertedFilename}.gif -")))
-    elif(os.name == "nt"):
-        os.system( str( PureWindowsPath(f"ffmpeg -i {videoFilePath} -v 0 -f yuv4mpegpipe - | ../bin/gifski-1_32_0.exe -o {app.config['GIF_UPLOAD_FOLDER']}/{convertedFilename}.gif -")))
-    else:
-        raise SystemError('Lista de sistemas compatíveis: Unix/Linux e Windows NT')
+def abort_app(text: str):
+    print(text)
+
+    input("Pressione Enter para sair. . .")
+    sys.exit(1)
+
+def videoToGif(videoPath: Path, outputName: str):
+    if not videoPath.is_file():
+        raise FileNotFoundError(f"Vídeo não encontrado: {videoPath!s}")
+    
+    outputPath = os.path.join(app.config["GIF_UPLOAD_FOLDER"], outputName+".gif")
+    cmd = [
+        "ffmpeg",
+        "-i", str(videoPath),
+        "-v", "0",
+        "-f", "yuv4mpegpipe",
+        "-",
+        "|",
+        GIFSKI_PATH,
+        "-o", outputPath,
+        "-"
+    ]
+
+    cmd_str = " ".join(cmd)
+    result = subprocess.run(cmd_str, shell=True, capture_output=True, text=True)
+    if result.returncode != 0:
+        raise RuntimeError(
+            f"Erro na conversão do vídeo para gif\n"
+            f"COMANDO: {cmd_str}\n"
+            f"STDOUT: {result.stdout}\n"
+            f"STDERR: {result.stderr}"
+        )
+
+    return os.path.abspath(outputPath)
 
 @app.route("/")
 def index():
@@ -41,15 +70,16 @@ def convert():
 
         # Salvar o arquivo
         filename = hex(secrets.randbits(64))[2:] # Gerar uma string hexadecimal aleatória de 16 caracteres
-        filepath = os.path.join(app.config['GIF_UPLOAD_FOLDER'], filename + ".mp4")
+        filepath = Path(os.path.join(app.config['GIF_UPLOAD_FOLDER'], filename + ".mp4"))
         file.save(filepath)
         videoToGif(filepath, filename)
 
         # Deleta o arquivo original
         try:
             os.remove(filepath)
-        except:
-            return "<h2>Ocorreu um erro inesperado, o video original sumiu do servidor misteriosamente. Se o problema persistir entra em contato comigo no e-mail<h2>"
+        except FileNotFoundError:
+            app.logger.warning(f'Vídeo não encontrado ao tentar remover: {os.path.abspath(filepath)}')
+            return "<h2>Ocorreu um erro inesperado: o vídeo original sumiu do servidor misteriosamente. Se o problema persistir, entre em contato comigo pelo e‑mail.</h2>"
         
         return redirect(url_for('get_gif', filename=filename+".gif"))
     
@@ -67,12 +97,15 @@ def get_gif(filename):
     return send_file(filepath, mimetype="image/gif")
 
 if __name__ == '__main__':
+    os.chdir(BASE_DIR)
+
+    if not os.name.startswith(("nt", "posix")):
+        abort_app('Sistema incompatível. Lista de sistemas compatíveis: Unix/Linux e Windows NT')
+
     if not os.path.exists(app.config['GIF_UPLOAD_FOLDER']):
         try:
             os.makedirs(app.config['GIF_UPLOAD_FOLDER'])
         except OSError as e:
-            print(f'Não foi possível criar o diretório "{app.config['GIF_UPLOAD_FOLDER']}": {e.strerror}. Verifique as permissões.')
-            os.system("pause")
-            exit(1)
+            abort_app(f'Não foi possível criar o diretório "{app.config['GIF_UPLOAD_FOLDER']}": {e.strerror}. Verifique as permissões.')
         
     app.run()
